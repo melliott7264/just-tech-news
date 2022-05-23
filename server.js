8cf8933f92751d68fb740d94fc8d38d8182ee345
Mark Elliott <markelliottva@gmail.com>
2022-05-23 07:52:25 -0400

    finished handlebars homepage

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-const express = require('express');
+const path = require("path");
 
-const routes = require('./routes');
+const express = require("express");
 
-const sequelize = require('./config/connection');
+// added for handlebars
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//added for handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// required to handle data streams
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+// sets up a static path for html/css code to be served
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 // turn on connection to database and server
 // change force to true to force a database/table drop and re-creation
 // make sure to change force back to false after database changes
-sequelize.sync({force: false}).then(() => {
-    app.listen(PORT, () => console.log('Now Listening'));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now Listening"));
 });
